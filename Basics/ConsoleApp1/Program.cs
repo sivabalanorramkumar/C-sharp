ffc3f2329a95b4ebbcf25ca43ef896f74219d1fd
sivabalanorramkumar <dsbtechtamil@gmail.com>
2025-05-24 13:19:03 -0700

    today i learned about condition statement

--- Basics/ConsoleApp1/Program.cs
+++ Basics/ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Data;
+using System.Diagnostics;
+using System.Linq.Expressions;
 using Microsoft.VisualBasic;
 using Microsoft.Win32.SafeHandles;
 
@@ -7,6 +9,13 @@ class Basic
 {
     public static void Main()
     {
+        //BASIC FOR DISPLAY OUTPUT AND GET INPUT FORM USER WE USE
+        //Console.WriteLine("enter you number");
+        //Console.Read();
+        //string name = "Siva";
+        //Console.WriteLine($"Hello there {name}!!!!!!");
+        //Console.WriteLine("Hello there "+name);
+        //Console.Read();
         //DATA type and variable declartion
 
         //string name;
@@ -35,14 +44,79 @@ class Basic
 
         //EXPLICT CONVERTIONS
 
-        int age, year ;
-        int cyear =2025;
-        Console.Write("Enter you date of year :");
-        year=Convert.ToInt32 (Console.ReadLine()); //we can use Convert.TO
-        //year = int.Parse(Console.ReadLine()); can we use parse for string
-        age = cyear - year;
-        Console.WriteLine("Your current age is " + age);
-       
+        //int age, year;
+        //int cyear = 2025;
+        //Console.Write("Enter you date of year :");
+        //year = Convert.ToInt32(Console.ReadLine()); //we can use Convert.TO
+        ////year = int.Parse(Console.ReadLine()); can we use parse for string
+        //age = cyear - year;
+        //Console.WriteLine("Your current age is " + age);
+
+        // OPERATORS ARITHEMETIC (+,-,/,%,*)
+        //float a = 4f;
+        //float  b= 7f;
+        //Console.WriteLine(a+b);
+        //Console.WriteLine(a-b);
+        //Console.WriteLine(a/b);
+        //Console.WriteLine(a*b);
+        //Console.WriteLine(a%b);
+
+        //COMPARSION
+        //Console.WriteLine(a==b);
+        //Console.WriteLine(a != b);
+        //Console.WriteLine(a<=b);
+        //Console.WriteLine(a>=b);
+        //Console.WriteLine(a>b);
+        //Console.WriteLine(a<b);
+        //Console.Read();
+
+
+        //accepting null able vaule
+        //int? a = null;
+        //Console.WriteLine(a);
+
+        //Console.Read();
+
+        //CONDITIONAL STATEMENT 
+        //int age;
+        //Console.WriteLine("APPLYING FOR VOTE ID");
+        //Console.WriteLine("--------------------\n");
+        //Console.Write("Enter you age :");
+        //age = int.Parse(Console.ReadLine());
+
+        //if (age < 18)
+        //{
+        //    Console.WriteLine("Your are not eligible to vote");
+        //}
+        //else if(age == 18){
+        //    Console.WriteLine("Your are eligible to vote and kindly come's with your DOB certificate");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Your are eligible to vote");
+        //}
+
+        //Console.ReadLine();
+
+        //ELSE IF LADDER
+        //int m1, m2, m3, m4, m5, total, percent;
+
+
+        //Console.Write("Enter your tamil mark :");
+        //m1 = Convert.ToInt32(Console.ReadLine());
+        //Console.Write("Enter your english mark :");
+        //m2 = int.Parse(Console.ReadLine());
+        //Console.Write("Enter your maths mark :");
+        //m3 = int.Parse(Console.ReadLine());
+        //Console.Write("Enter your social  mark :");
+        //m4 = int.Parse(Console.ReadLine());
+        //Console.Write("Enter your science mark :");
+        //m5 = int.Parse(Console.ReadLine());
+        //total = m1 + m2 + m3 + m4 + m5;
+        //percent = total * 100 / 500;
+        //Console.WriteLine(percent)
+        //Console.ReadLine();
+
 
     }
 }
--- Basics/ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ Basics/ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29257aeadc3d83107ec88e779a16fec436976275")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
